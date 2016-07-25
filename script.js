85745df21821ec29758a1b6434860d34a8621121
Vladimir Kamuz <v.kamuz@gmail.com>
2016-07-25 14:21:09 +0300

    Creating DOM elements with events - Finish

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.querySelector('.grid').addEventListener('mouseover', function(e) {
   if (e.target.tagName === 'IMG') {
 
+    var myElement = document.createElement('div');
+    myElement.className = 'preview';
+    e.target.parentNode.appendChild(myElement);
 
-  } // check to see that I clicked on IMG only
-}, false); // click event
+    var myImg = document.createElement('img');
+    var imgLoc = e.target.src;
+    myImg.src = imgLoc.substr(0, imgLoc.length-7) + '.jpg';
+    myElement.appendChild(myImg);
+  }
+}, false);
