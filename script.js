cd92f3ae49d49b714cfbfcda9f99287424a4d26e
Vladimir Kamuz <v.kamuz@gmail.com>
2016-07-25 11:46:14 +0300

    Cleaning up event issues

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.querySelector('.grid').addEventListener('click', function(e){
-  var removeTarget = e.target.parentNode;
-  removeTarget.parentNode.removeChild(removeTarget);
+  console.dir(e.target);
+  if(e.target.tagName === "IMG"){
+    var howmany = this.querySelectorAll('li').length;
+    if (howmany > 1){
+      var removeTarget = e.target.parentNode;
+      removeTarget.parentNode.removeChild(removeTarget);
+    } else{
+      var photoTitle = e.target.alt;
+      document.querySelector('#art p').innerHTML = "<p> You've picked: " + photoTitle + "</p>";
+    }
+  }
 }, false);
