fe780d5e34e7c9a6fa62de28084575ce5040d16e
Vladimir Kamuz <v.kamuz@gmail.com>
2016-09-04 18:05:42 +0300

    Handling media pauses

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 var jukebox = document.querySelector("ul.player");
 jukebox.addEventListener("click", function(e){
-  var audioPlayer = document.createElement('audio');
   var soundName = e.target.getAttribute('data-src');
-  audioPlayer.id = 'player';
-  e.target.id = 'playing';
-  audioPlayer.src = soundName;
-  document.body.appendChild(audioPlayer);
-  audioPlayer.play();
+  var soundPlaying = document.querySelector("#player");
+  if (soundPlaying) {
+    if (soundPlaying.paused) {
+      soundPlaying.play();
+      e.target.id = 'playing';
+    }
+    else {
+      soundPlaying.pause();
+      e.target.id = 'paused';
+    }
+  } else {
+    var audioPlayer = document.createElement('audio');
+    audioPlayer.id = 'player';
+    e.target.id = 'playing';
+    audioPlayer.src = soundName;
+    document.body.appendChild(audioPlayer);
+    audioPlayer.play();
 
-  audioPlayer.addEventListener('ended', function(){
-    audioPlayer.parentNode.removeChild(audioPlayer);
-    e.target.id = '';
-  }, false);
+    audioPlayer.addEventListener('ended', function(){
+      audioPlayer.parentNode.removeChild(audioPlayer);
+      e.target.id = '';
+    }, false);
+  }
 }), false;
